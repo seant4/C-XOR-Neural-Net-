2cfba5ca60bbd4140c2e2f2fdf298d54b32d113f
Sean <seanjtheisen@gmail.com>
2023-05-27 01:07:46 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,158 +1,139 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 #include "util.h"
 
-#define features 2
-#define hiddenNodes 2
-#define outputNodes 1
-#define samples 4
+#define FEATURES 2
+#define SAMPLES 4
+#define NEURONS 2
+#define OUTPUTS 1
 
-int forward_prop(double* hiddenLayerBias, double x_train[samples][features], 
-                double hiddenWeights[features][hiddenNodes], double* hiddenLayer, double* outputLayer, double* outputLayerBias, 
-                double outputWeights[hiddenNodes][outputNodes], double y_train[samples][1], int val){
-
-    //Compute hidden layer
-    double node = 0;
-    for(int i = 0; i < hiddenNodes; i++){
-        for(int k = 0; k < features; k++){
-            node += (x_train[val][k] * hiddenWeights[k][i]) + hiddenLayerBias[i];
-        }
-        hiddenLayer[i] = sigmoid(node);
-    }
-
-    //Compute output layer
-    node = 0;
-    for(int i = 0; i < outputNodes; i++){
-        for(int k = 0; k < hiddenNodes; k++){
-            node += (hiddenLayer[k] * outputWeights[k][i]) + outputLayerBias[i];
-        }
-        outputLayer[i] = sigmoid(node);
-    }
-    return 0;
-}
-
-int backward_prop(double y_train[samples][1], double* outputLayer, double outputWeights[hiddenNodes][outputNodes], 
-                double* hiddenLayer, double* delta, double* deltaHidden, int val){
-    //Calculate error of output layer
-    for(int j = 0; j < outputNodes; j++){
-        double err = (y_train[val][j] - outputLayer[j]);
-        delta[j] = err * dSigmoid(outputLayer[j]);
-    }
-
-    //Calculate error of hidden layer
-    for(int j = 0; j < hiddenNodes; j++){
-        double err = 0.0f;
-        for(int k = 0; k < outputNodes; k++){
-            err += delta[k] * outputWeights[j][k];
-        }
-        deltaHidden[j] = err * dSigmoid(hiddenLayer[j]);
-    }
-    return 0;
-}
-
-int update_params(double* outputLayerBias, double* delta, double alpha, double outputWeights[hiddenNodes][outputNodes], 
-                double* hiddenLayer, double* hiddenLayerBias, double* deltaHidden,
-                double hiddenWeights[features][hiddenNodes], double x_train[samples][features], int val){
-    //Update the output layer
-    for(int j = 0; j < outputNodes; j++){
-        outputLayerBias[j] += delta[j] * alpha;
-        for(int k = 0; k < hiddenNodes; k++){
-            outputWeights[k][j] += hiddenLayer[k] * delta[j] * alpha;
+void init_params(int n1, int m1, int n2, int m2, double W1[n1][m1], double W2[n2][m1]){
+    for(int i = 0; i < n1; i++){
+        for(int j = 0; j < m1; j++){
+            W1[i][j] = ((double)rand()) / ((double)RAND_MAX);
         }
     }
-    //Output the hidden layer
-    for(int j = 0; j < hiddenNodes; j++){
-        hiddenLayerBias[j] += deltaHidden[j] * alpha;
-        for(int k = 0; k < features; k++){
-            hiddenWeights[k][j] += x_train[val][k] * deltaHidden[j] * alpha;
+
+    for(int i = 0; i < n2; i++){
+        for(int j = 0; j < m2; j++){
+            W2[i][j] = ((double)rand()) / ((double)RAND_MAX);
         }
     }
 }
 
-//Initialize our random parameters
-double init_params(){
-    return ((double)rand()) / ((double)RAND_MAX);
+void forward_prop(double W1[NEURONS][FEATURES], double W2[OUTPUTS][NEURONS], double X[FEATURES][SAMPLES], double Z1[NEURONS][SAMPLES], 
+                double A1[NEURONS][SAMPLES], double Z2[OUTPUTS][SAMPLES], double A2[OUTPUTS][SAMPLES]){
+
+    matrix_dot(2,2,2,4,W1,X,Z1);
+    for(int i = 0; i < NEURONS; i++){
+        for(int j = 0; j < SAMPLES; j++){
+            A1[i][j] = sigmoid(Z1[i][j]);
+        }
+    }
+
+    matrix_dot(1,2,2,4,W2,A1,Z2);
+    for(int i = 0; i < OUTPUTS; i++){
+        for(int j = 0; j < SAMPLES; j++){
+            A2[i][j] = sigmoid(Z2[i][j]);
+        }
+    }
+}
+
+void back_prop(double W1[NEURONS][FEATURES], double W2[OUTPUTS][NEURONS], double Z1[NEURONS][SAMPLES], double A1[NEURONS][SAMPLES], 
+                double Z2[OUTPUTS][SAMPLES], double A2[OUTPUTS][SAMPLES], double dW1[NEURONS][FEATURES], double dW2[OUTPUTS][NEURONS], 
+                double dZ1[NEURONS][SAMPLES], double dZ2[OUTPUTS][SAMPLES], double X[FEATURES][SAMPLES], double Y[OUTPUTS][SAMPLES]){
+
+    for(int i = 0; i < OUTPUTS; i++){
+        for(int j = 0; j < SAMPLES; j++){
+            dZ2[i][j] = A2[i][j] - Y[i][j];
+        }
+    }
+    double A1T[SAMPLES][NEURONS];
+    transposeMatrix(NEURONS, SAMPLES, A1, A1T);
+    matrix_dot(OUTPUTS, SAMPLES, SAMPLES, NEURONS, dZ2, A1T, dW2);
+    for(int i = 0; i < OUTPUTS; i++){
+        for(int j = 0; j < NEURONS; j++){
+            dW2[i][j] = dW2[i][j] / SAMPLES;
+        }
+    }
+    double W2T[NEURONS][OUTPUTS];
+    transposeMatrix(NEURONS, OUTPUTS, W2, W2T);
+    matrix_dot(NEURONS, OUTPUTS, OUTPUTS, SAMPLES, W2T, dZ2, dZ1);
+    for(int i = 0; i < NEURONS; i++){
+        for(int j = 0; j < SAMPLES; j++){
+            dZ1[i][j] = dZ1[i][j] * (A1[i][j]*(1-A1[i][j]));
+        }
+    }
+    double XT[SAMPLES][FEATURES];
+    transposeMatrix(FEATURES, SAMPLES, X, XT);
+    matrix_dot(NEURONS, SAMPLES, SAMPLES, FEATURES, dZ1, XT, dW1);
+    for(int i = 0; i < NEURONS; i++){
+        for(int j = 0; j < FEATURES; j++){
+            dW1[i][j] = dW1[i][j] / SAMPLES;
+        }
+    }
+}
+
+void update_params(double W1[NEURONS][FEATURES], double W2[OUTPUTS][NEURONS], double dW1[NEURONS][FEATURES], double dW2[OUTPUTS][NEURONS], double alpha){
+    for(int i = 0; i < OUTPUTS; i++){
+        for(int j = 0; j < NEURONS; j++){
+            W2[i][j] = W2[i][j] - alpha * dW2[i][j];
+        }
+    }
+    for(int i = 0; i < NEURONS; i++){
+        for(int j = 0; j < FEATURES; j++){
+            W1[i][j] = W1[i][j] - alpha * dW1[i][j]; 
+        }
+    }
+}
+
+void forward_prop_test(double W1[NEURONS][FEATURES], double W2[OUTPUTS][NEURONS], double X[FEATURES][1], double Z1[NEURONS][1], 
+                double A1[NEURONS][1], double Z2[OUTPUTS][1], double A2[OUTPUTS][1]){
+
+    matrix_dot(2,2,2,1,W1,X,Z1);
+    for(int i = 0; i < NEURONS; i++){
+        for(int j = 0; j < 1; j++){
+            A1[i][j] = sigmoid(Z1[i][j]);
+        }
+    }
+
+    matrix_dot(1,2,2,1,W2,A1,Z2);
+    for(int i = 0; i < OUTPUTS; i++){
+        for(int j = 0; j < 1; j++){
+            A2[i][j] = sigmoid(Z2[i][j]);
+        }
+    }
 }
 
 int main(){
-    const double alpha = 1.0f;
+    //Define weight matricies
+    double W1[NEURONS][FEATURES];
+    double W2[OUTPUTS][NEURONS];
+    double Z1[NEURONS][SAMPLES];
+    double A1[NEURONS][SAMPLES];
+    double Z2[OUTPUTS][SAMPLES];
+    double A2[OUTPUTS][SAMPLES];
 
-    //Initialize layers
-    double hiddenLayer[hiddenNodes];
-    double outputLayer[outputNodes];
-    double hiddenLayerBias[hiddenNodes];
-    double outputLayerBias[outputNodes];
-    double hiddenWeights[features][hiddenNodes];
-    double outputWeights[hiddenNodes][outputNodes];
+    //Define training data
+    double x_train[FEATURES][SAMPLES] = {{0,0,1,1},{0,1,0,1}};
+    double y_train[OUTPUTS][SAMPLES] = {{0,1,1,0}};
 
-    //Define our data
-    double x_train[samples][features] = {
-        {0.0f,0.0f},
-        {1.0f, 0.0f},
-        {0.0f, 1.0f},
-        {1.0f, 1.0f},
-    };
+    //Define delta matricies
+    double dW1[NEURONS][FEATURES];
+    double dW2[OUTPUTS][NEURONS];
+    double dZ1[NEURONS][SAMPLES];
+    double dA1[NEURONS][SAMPLES];
+    double dZ2[OUTPUTS][SAMPLES];
+    double dA2[OUTPUTS][SAMPLES];
 
-    //Define our outputs
-    double y_train[samples][outputNodes] = {
-        {0.0f},
-        {1.0f},
-        {1.0f},
-        {0.0f}
-    };
+    init_params(2,2,1,2,W1,W2);
 
-    //Fill in our layers
-    for(int i = 0; i < features; i++){
-        for(int j = 0; j < hiddenNodes; j++){
-            hiddenWeights[i][j] = init_params();
-        }
+    for(int i = 0; i < 10000; i++){
+        forward_prop(W1,W2,x_train,Z1,A1,Z2,A2);
+        back_prop(W1,W2,Z1,A1,Z2,A2,dW1,dW2,dZ1,dZ2,x_train,y_train);
+        update_params(W1,W2,dW1,dW2,0.5);
     }
-
-    for(int i = 0; i < features; i++){
-        for(int j = 0; j < outputNodes; j++){
-            outputWeights[i][j] = init_params();
-        }
-    }
-
-    for(int i = 0; i < outputNodes; i++){
-        outputLayerBias[i] = init_params();
-    }
-
-    //Define random order of training to prevent over fitting
-    int trainingSetOrder[] = {0,1,2,3};
-
-    //Define epochs
-    int epochs = 10000;
-
-    //Begin training from epochs
-    for(int i = 0; i < epochs; i++){
-        //Train by each sample
-        for(int x = 0; x < samples; x++){
-            //Shuffle order of data
-            shuffle(trainingSetOrder, samples);
-            //Grab Sample to forward propogate
-            int val = trainingSetOrder[x];
-            //Forward Propogate
-            forward_prop(hiddenLayerBias, x_train, hiddenWeights, 
-                        hiddenLayer, outputLayer, outputLayerBias, outputWeights, y_train, val);
-            //Define error arrays
-            double delta[outputNodes];
-            double deltaHidden[hiddenNodes];
-            //Back propogate
-            backward_prop(y_train, outputLayer, outputWeights, hiddenLayer, delta, deltaHidden, val);
-            //Apply changes
-            update_params(outputLayerBias, delta, alpha, outputWeights, hiddenLayer, hiddenLayerBias, deltaHidden, hiddenWeights, x_train, val);
-
-        }
-    }
-    //Test model on all 4 XOR operations
-    forward_prop(hiddenLayerBias, x_train, hiddenWeights, hiddenLayer, outputLayer, outputLayerBias, outputWeights, y_train, 0);
-    print_matrix(outputNodes, 1, outputLayer);
-    forward_prop(hiddenLayerBias, x_train, hiddenWeights, hiddenLayer, outputLayer, outputLayerBias, outputWeights, y_train, 1);
-    print_matrix(outputNodes, 1, outputLayer);
-    forward_prop(hiddenLayerBias, x_train, hiddenWeights, hiddenLayer, outputLayer, outputLayerBias, outputWeights, y_train, 2);
-    print_matrix(outputNodes, 1, outputLayer);
-    forward_prop(hiddenLayerBias, x_train, hiddenWeights, hiddenLayer, outputLayer, outputLayerBias, outputWeights, y_train, 3);
-    print_matrix(outputNodes, 1, outputLayer);
+    print_matrix(1,4,A2);
+    return 0;
 }
--- util.h
+++ util.h
@@ -1,6 +1,57 @@
 #include <stdio.h>
 #include <math.h>
 
+#define FEATURES 2
+#define SAMPLES 4
+
+// Function to transpose a matrix
+void transposeMatrix(int m, int n, double matrix[m][n], double transposed[n][m]) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            transposed[j][i] = matrix[i][j];
+        }
+    }
+}
+
+void sumColumns(int rows, int columns, double matrix[rows][columns], double sums[]) {
+    for (int j = 0; j < columns; j++) {
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            sum += matrix[i][j];
+        }
+        sums[j] = sum;
+    }
+}
+
+int matrix_dot(int m1, int n1, int m2, int n2, double a[m1][n1], double b[m2][n2], double result[m1][n2]){
+    for(int i = 0; i < m1; i++){
+        for(int j = 0; j < n2; j++){
+            result[i][j] = 0;
+            for(int k = 0; k < m2; k++){
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    return 0;
+}
+
+int matrix_vector_add(int m, int n, double a[m][n], double v[n]){
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            a[m][n] += v[m];
+        }
+    }
+    return 0;
+}
+
+int matrix_vector_subtract(int m, int n, double a[m][n], double v[n], double result[m][n]){
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            result[m][n] = a[m][n] - v[m];
+        }
+    }
+    return 0;
+}
 int print_matrix(int m, int n, double a[m][n]){
     for(int i = 0; i < m; i++){
         for(int j = 0; j < n; j++){
@@ -10,21 +61,60 @@ int print_matrix(int m, int n, double a[m][n]){
     return 0;
 }
 
-double sigmoid(double x){
-    return 1 / (1+exp(-1 * x));
-}
-
-double dSigmoid(double x){
-    return x * (1-x);
-}
-
-void shuffle(int *array, int n){
-    if(n > 1){
-        for(int i = 0; i < n - 1; i++){
-            int j = 1 + rand() / (RAND_MAX / (n-i) + 1);
-            int t = array[j];
-            array[j] = array[i];
-            array[i] = t;
-        }
+double relu(double x){
+    if(x > 0){
+        return x;
     }
+    return 0;
+}
+
+double relu_deriv(double x){
+    if(x > 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+double sigmoid(double x){
+    return (1 / (1 + exp(-1 * x)));
+}
+
+double sigmoid_deriv(double x){
+    return (sigmoid(x) * (1 - sigmoid(x)));
+}
+
+void findLargestIndexForEachColumn(int rows, int cols, double matrix[rows][cols], double result[cols]) {
+    for (int j = 0; j < cols; j++) {
+        int maxVal = matrix[0][j];
+        int maxIndex = 0;
+
+        for (int i = 1; i < cols; i++) {
+            if (matrix[i][j] > maxVal) {
+                maxVal = matrix[i][j];
+                maxIndex = i;
+            }
+        }
+
+        result[j] = maxIndex;
+    }
+}
+
+void softmax(double *input, int input_len) {
+  double m = -INFINITY;
+  for (int i = 0; i < input_len; i++) {
+    if (input[i] > m) {
+      m = input[i];
+    }
+  }
+
+  double sum = 0.0;
+  for (int i = 0; i < input_len; i++) {
+    sum += expf(input[i] - m);
+  }
+
+  double offset = m + logf(sum);
+  for (int i = 0; i < input_len; i++) {
+    input[i] = expf(input[i] - offset);
+  }
 }
